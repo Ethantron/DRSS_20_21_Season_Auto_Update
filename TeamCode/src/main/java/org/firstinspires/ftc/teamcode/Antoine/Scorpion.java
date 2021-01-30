f7946fc0578606490c5ab7f89cb966bc5802fe2a
Ethan <48032872+ethantron@users.noreply.github.com>
2021-01-30 14:38:45 -0500

    Updated Scorpion to have TeleDrive
    Signed-off-by: Ethan <48032872+Ethantron@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Antoine/Scorpion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Antoine/Scorpion.java
@@ -16,6 +16,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +32,313 @@ public class Scorpion extends LinearOpMode{
 
 	private final InitHardware robot = new InitHardware();  //Load hardware from hardware map
 
+	private DatagramSocket socket;
+	private boolean canRunGamepadThread;
+	private Thread gamepadHandler;
+
 	WebcamName webcamName = null;
+
+	private void startGamepadHandlerThread() {
+		telemetry.setAutoClear(true);
+		gamepadHandler = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (canRunGamepadThread) {
+					String gamepadAction = "";
+					try {
+						byte[] buffer = new byte[1024];
+						DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+						socket.receive(response);
+						gamepadAction = new String(buffer, 0, response.getLength());
+					} catch (Exception ignore) {
+
+					}
+					telemetry.addData("TeleDrive", gamepadAction);
+					telemetry.update();
+					if (!gamepadAction.isEmpty()) {
+						if (gamepadAction.contains("G1")) {
+							if (gamepadAction.contains("_A")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.a = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.a = false;
+								}
+							}
+							if (gamepadAction.contains("_B")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.b = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.b = false;
+								}
+							}
+							if (gamepadAction.contains("_X")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.x = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.x = false;
+								}
+							}
+							if (gamepadAction.contains("_Y")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.y = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.y = false;
+								}
+							}
+							if (gamepadAction.contains("_D")) {
+								if (gamepadAction.contains("UP")) {
+									gamepad1.dpad_up = true;
+									gamepad1.dpad_down = false;
+									gamepad1.dpad_left = false;
+									gamepad1.dpad_right = false;
+								}
+								if (gamepadAction.contains("DOWN")) {
+									gamepad1.dpad_down = true;
+									gamepad1.dpad_up = false;
+									gamepad1.dpad_left = false;
+									gamepad1.dpad_right = false;
+								}
+								if (gamepadAction.contains("LEFT")) {
+									gamepad1.dpad_left = true;
+									gamepad1.dpad_up = false;
+									gamepad1.dpad_down = false;
+									gamepad1.dpad_right = false;
+								}
+								if (gamepadAction.contains("RIGHT")) {
+									gamepad1.dpad_right = true;
+									gamepad1.dpad_up = false;
+									gamepad1.dpad_down = false;
+									gamepad1.dpad_left = false;
+								}
+								if (gamepadAction.contains("NONE")) {
+									gamepad1.dpad_up = false;
+									gamepad1.dpad_down = false;
+									gamepad1.dpad_left = false;
+									gamepad1.dpad_right = false;
+								}
+							}
+							if (gamepadAction.contains("_RT")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.right_trigger = 1.0f;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.right_trigger = 0.0f;
+								}
+							}
+							if (gamepadAction.contains("_LT")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.left_trigger = 1.0f;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.left_trigger = 0.0f;
+								}
+							}
+							if (gamepadAction.contains("_RB")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.right_bumper = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.right_bumper = false;
+								}
+							}
+							if (gamepadAction.contains("_LB")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.left_bumper = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.left_bumper = false;
+								}
+							}
+							if (gamepadAction.contains("_RS")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.right_stick_button = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.right_stick_button = false;
+								}
+							}
+							if (gamepadAction.contains("_LS")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.left_stick_button = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.left_stick_button = false;
+								}
+							}
+							if (gamepadAction.contains("_START")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.start = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.start = false;
+								}
+							}
+							if (gamepadAction.contains("_BACK")) {
+								if (gamepadAction.contains("P")) {
+									gamepad1.back = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad1.back = false;
+								}
+							}
+							if (gamepadAction.contains("_Jx")) {
+								gamepad1.left_stick_x = Float.parseFloat(gamepadAction.replace("G1_Jx_", ""));
+							}
+							if (gamepadAction.contains("_Jy")) {
+								gamepad1.left_stick_y = Float.parseFloat(gamepadAction.replace("G1_Jy_", ""));
+							}
+							if (gamepadAction.contains("_Jz")) {
+								gamepad1.right_stick_x = Float.parseFloat(gamepadAction.replace("G1_Jz_", ""));
+							}
+							if (gamepadAction.contains("_Jrz")) {
+								gamepad1.right_stick_y = Float.parseFloat(gamepadAction.replace("G1_Jrz_", ""));
+							}
+						}
+						if (gamepadAction.contains("G2")) {
+							if (gamepadAction.contains("_A")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.a = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.a = false;
+								}
+							}
+							if (gamepadAction.contains("_B")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.b = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.b = false;
+								}
+							}
+							if (gamepadAction.contains("_X")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.x = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.x = false;
+								}
+							}
+							if (gamepadAction.contains("_Y")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.y = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.y = false;
+								}
+							}
+							if (gamepadAction.contains("_D")) {
+								if (gamepadAction.contains("UP")) {
+									gamepad2.dpad_up = true;
+									gamepad2.dpad_down = false;
+									gamepad2.dpad_left = false;
+									gamepad2.dpad_right = false;
+								}
+								if (gamepadAction.contains("DOWN")) {
+									gamepad2.dpad_down = true;
+									gamepad2.dpad_up = false;
+									gamepad2.dpad_left = false;
+									gamepad2.dpad_right = false;
+								}
+								if (gamepadAction.contains("LEFT")) {
+									gamepad2.dpad_left = true;
+									gamepad2.dpad_up = false;
+									gamepad2.dpad_down = false;
+									gamepad2.dpad_right = false;
+								}
+								if (gamepadAction.contains("RIGHT")) {
+									gamepad2.dpad_right = true;
+									gamepad2.dpad_up = false;
+									gamepad2.dpad_down = false;
+									gamepad2.dpad_left = false;
+								}
+								if (gamepadAction.contains("NONE")) {
+									gamepad2.dpad_up = false;
+									gamepad2.dpad_down = false;
+									gamepad2.dpad_left = false;
+									gamepad2.dpad_right = false;
+								}
+							}
+							if (gamepadAction.contains("_RT")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.right_trigger = 1.0f;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.right_trigger = 0.0f;
+								}
+							}
+							if (gamepadAction.contains("_LT")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.left_trigger = 1.0f;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.left_trigger = 0.0f;
+								}
+							}
+							if (gamepadAction.contains("_RB")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.right_bumper = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.right_bumper = false;
+								}
+							}
+							if (gamepadAction.contains("_LB")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.left_bumper = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.left_bumper = false;
+								}
+							}
+							if (gamepadAction.contains("_RS")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.right_stick_button = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.right_stick_button = false;
+								}
+							}
+							if (gamepadAction.contains("_LS")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.left_stick_button = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.left_stick_button = false;
+								}
+							}
+							if (gamepadAction.contains("_START")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.start = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.start = false;
+								}
+							}
+							if (gamepadAction.contains("_BACK")) {
+								if (gamepadAction.contains("P")) {
+									gamepad2.back = true;
+								} else if (gamepadAction.contains("R")) {
+									gamepad2.back = false;
+								}
+							}
+							if (gamepadAction.contains("_Jx")) {
+								gamepad2.left_stick_x = Float.parseFloat(gamepadAction.replace("G2_Jx_", ""));
+							}
+							if (gamepadAction.contains("_Jy")) {
+								gamepad2.left_stick_y = Float.parseFloat(gamepadAction.replace("G2_Jy_", ""));
+							}
+							if (gamepadAction.contains("_Jz")) {
+								gamepad2.right_stick_x = Float.parseFloat(gamepadAction.replace("G2_Jz_", ""));
+							}
+							if (gamepadAction.contains("_Jrz")) {
+								gamepad2.right_stick_y = Float.parseFloat(gamepadAction.replace("G2_Jrz_", ""));
+							}
+						}
+					}
+				}
+				gamepadHandler.interrupt();
+			}
+		});
+		gamepadHandler.setName("Gamepad Handler Thread");
+		gamepadHandler.setPriority(Thread.NORM_PRIORITY);
+		gamepadHandler.start();
+	}
+
 
 	@Override
 	public void runOpMode() throws InterruptedException{
+
+		String address = "192.168.43.1"; //Check "Program and Manage" tab on the Driver Station and verify the IP address
+		int port = 12345; //Change as needed
+		canRunGamepadThread = false;
+
+		try {
+			this.socket = new DatagramSocket(port);
+		} catch (Exception ex) {
+			telemetry.addData("Exception: ", ex.getMessage());
+		}
 
 		robot.init(hardwareMap); //Initialize hardware
 		telemetry.addData("Drive Train", "Initialized");      // Adds telemetry to the screen to show that the drive train is initialized
@@ -152,10 +457,15 @@ public class Scorpion extends LinearOpMode{
 		telemetry.addData("Vuforia", "Initialized");      // Adds telemetry to the screen to show that the drive train is initialized
 		telemetry.addData("Launcher", "Initialized");      // Adds telemetry to the screen to show that the drive train is initialized
 		telemetry.addData("Status", "Ready");                 // Adds telemetry to the screen to show that the robot is ready
+		telemetry.addData("Connect your server to " + address + ":" + port, "");
 		telemetry.addData("Press Play to Start", "TeleOp");    // Adds telemetry to the screen to tell the drivers that the code is ready to start
 		telemetry.update();
 
 		waitForStart();
+
+		canRunGamepadThread = true;
+
+		startGamepadHandlerThread();
 
 		targetsUltimateGoal.activate();
 
@@ -359,6 +669,9 @@ public class Scorpion extends LinearOpMode{
 
 		targetsUltimateGoal.deactivate();
 		//robot.deactivateTFOD();
+
+		canRunGamepadThread = false;
+		socket.close();
 	}
 
 	private void autoAim(float distance) throws InterruptedException{
